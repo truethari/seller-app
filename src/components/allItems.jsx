e2c17c1be3d4ba8c5d412b7c9ad13bd8b61faf9e
Tharindu N <tharindu.nm@yahoo.com>
2022-04-01 17:44:48 +0530

    Notification

--- src/components/allItems.jsx
+++ src/components/allItems.jsx
@@ -1,63 +1,104 @@
 import React, { Component } from "react";
 import { Button, Table } from "react-bootstrap";
 import { getAllItems } from "../services/fakeItems";
+import Notification from "./common/notification";
 
 class AllItems extends Component {
   state = {
     items: getAllItems(),
+    sellNotificationOpen: false,
+    selectedItemObject: {},
+    action: "",
   };
+
+  setModalShow = () => {
+    const sellNotificationOpen = false;
+    this.setState({ sellNotificationOpen });
+  };
+
+  handleSellButton = (object) => {
+    const sellNotificationOpen = true;
+    const selectedItemObject = object;
+    const action = "sell";
+    this.setState({ sellNotificationOpen, selectedItemObject, action });
+  };
+
+  handleInfoButton = (object) => {
+    const sellNotificationOpen = true;
+    const selectedItemObject = object;
+    const action = "info";
+    this.setState({ sellNotificationOpen, selectedItemObject, action });
+  };
+
+  handleRemoveButton = (object) => {
+    const sellNotificationOpen = true;
+    const selectedItemObject = object;
+    const action = "delete";
+    this.setState({ sellNotificationOpen, selectedItemObject, action });
+  };
+
   render() {
     return (
-      <div className="table-container">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th width="3%">#</th>
-              <th width="5%">ID</th>
-              <th width="7%">Item Code</th>
-              <th width="30%">Name</th>
-              <th width="10%">Price</th>
-              <th width="15%">Description</th>
-              <th width="20%"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.items.map((e, index) => (
-              <tr key={e.id}>
-                <td>{index + 1}</td>
-                <td>{e.id}</td>
-                <td>{e.itemCode}</td>
-                <td>{e.brandName + " - " + e.productName}</td>
-                <td>{e.price}</td>
-                <td>{e.description}</td>
-                <td>
-                  <Button
-                    style={{ marginLeft: 5, marginRight: 5 }}
-                    variant="warning"
-                    size="sm"
-                  >
-                    Sell
-                  </Button>
-                  <Button
-                    style={{ marginLeft: 0, marginRight: 5 }}
-                    variant="primary"
-                    size="sm"
-                  >
-                    Info
-                  </Button>
-                  <Button
-                    style={{ marginLeft: 0, marginRight: 5 }}
-                    variant="danger"
-                    size="sm"
-                  >
-                    Remove
-                  </Button>
-                </td>
+      <React.Fragment>
+        <div className="table-container">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th width="3%">#</th>
+                <th width="5%">ID</th>
+                <th width="7%">Item Code</th>
+                <th width="30%">Name</th>
+                <th width="10%">Price</th>
+                <th width="15%">Description</th>
+                <th width="20%"></th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+            </thead>
+            <tbody>
+              {this.state.items.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{index + 1}</td>
+                  <td>{item.id}</td>
+                  <td>{item.itemCode}</td>
+                  <td>{item.brandName + " - " + item.productName}</td>
+                  <td>{item.price}</td>
+                  <td>{item.description}</td>
+                  <td>
+                    <Button
+                      style={{ marginLeft: 5, marginRight: 5 }}
+                      variant="warning"
+                      size="sm"
+                      onClick={() => this.handleSellButton(item)}
+                    >
+                      Sell
+                    </Button>
+                    <Button
+                      style={{ marginLeft: 0, marginRight: 5 }}
+                      variant="primary"
+                      size="sm"
+                      onClick={() => this.handleInfoButton(item)}
+                    >
+                      Info
+                    </Button>
+                    <Button
+                      style={{ marginLeft: 0, marginRight: 5 }}
+                      variant="danger"
+                      size="sm"
+                      onClick={() => this.handleRemoveButton(item)}
+                    >
+                      Remove
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <Notification
+          show={this.state.sellNotificationOpen}
+          onHide={this.setModalShow}
+          actionInfo={this.state.action}
+        />
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/common/notification.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const Notification = (props) => {
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.onHide}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary">Yes</Button>
+        <Button variant="danger" onClick={props.onHide}>
+          No
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default Notification;
